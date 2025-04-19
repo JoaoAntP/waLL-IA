e597873c424619598a2bd34889f9e2196bce5395
Kyon <139816800+JoaoAntP@users.noreply.github.com>
2025-04-19 18:19:26 -0300

    Add files via upload

--- waLL-1.0.py
+++ waLL-1.0.py
@@ -10,13 +10,13 @@
 
 
 nome_usuario = input("Qual seu nome ?")
-codigo_usario = "1"
+acesso_usuario = input("Qual seu codigo de acesso ?")
+codigo_usuario = acesso_usuario
 print("""
 1-Adulto
 2-Criança
 3-Idoso
 """)
-
 modo_comuni = (input("Qual modo você deseja"))
 if modo_comuni == "2":
     print("Seja bem vindo Meu Anjinho {} qual e sua necessidade".format(nome_usuario))
@@ -30,12 +30,50 @@ if modo_comuni == "2":
     """)
     tipo_necessi = input("Qual seu tipo de necessidade ?")
     if tipo_necessi == "1":
-        print("i")
-    elif tipo_necessi =="2":
-        print("t")
-    elif tipo_necessi == "3":
-        print("h")
+        print(""""
+        
+        1-Assitencia (Urgente)
+        2-Dor no corpo
+        3-Doença Contagiosa
+        4-Fratura
+        5-Remedio
+        
+        
+        """)
+    tp_ncf = input("Qual seu numero você deseja selecionar")
+    if tp_ncf == "1":
+        print("Um aviso foi enviado!, Um medico chegara em breve")
+    elif tp_ncf == "2":
+        print(""" Em qual região do seu corpo a dor se encontra ?
+        
+        1-Cabeça
+        2-Costa
+        3-Peito
+        4-Braços
+        5-Pernas
+        
+        """)
+        local_dor = input("Qual das opções acima")
+        if local_dor == "1":
+            print("a")
+        elif local_dor == "2":
+            print("b")
+        elif local_dor == "3":
+            print("c")
+        elif local_dor == "4":
+            print("d")
+        elif local_dor == "5":
+            print("e")
+        else:
+            print("Erro")
+    elif tipo_necessi =="3":
+        nivel_parentesco = input("Quem estava com você Pai/Mãe/Avô")
+        nome_pais = input("Nome do seu responsalvel")
+        msg1= "Vamos chamar seu(a) {} Vamos chamar por {} nos altos falantes".format(nivel_parentesco, nome_pais)
+        print(msg1)
     elif tipo_necessi == "4":
+        print("h")
+    elif tipo_necessi == "5":
         print("m")
     else:
         print("Escolha de uma opções acima")
@@ -59,7 +97,7 @@ elif modo_comuni == "1":
         print("h")
     elif tipo_necessi == "4":
         input("Qual seu codigo ?")
-    if codigo_usario == "1":
+    if codigo_usuario == "1":
         print("""Seus Horarios são 
         
         Seg|10:40-12:20|Lazer
@@ -68,7 +106,7 @@ elif modo_comuni == "1":
         """)
     else:
         print("Escolha de uma opções acima")
-else:
+elif modo_comuni == "3":
     print("Seja Bem Vindx Senhora(o) {}".format(nome_usuario))
     print("Seja Bem Vindx {}".format(nome_usuario))
     print("Seja bem vindo {} qual e sua necessidade".format(nome_usuario))
@@ -93,3 +131,5 @@ else:
         Quinta|17:00-20:25|Sessão de Cinema""")
     else:
         print("Escolha de uma opções acima")
+else:
+    print("Erro")
